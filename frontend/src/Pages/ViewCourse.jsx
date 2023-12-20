94a2339c4325fafb7aa6dc69fd21b67de5354998
VishnuVelukutty <vishnuvelukutty26@gmail.com>
2023-12-20 23:28:16 +0530

    Added CORS+Removed modal

--- frontend/src/Pages/ViewCourse.jsx
+++ frontend/src/Pages/ViewCourse.jsx
@@ -48,23 +48,9 @@ const ViewCourse = () => {
         .catch(err => console.log("Error occurred", err));
     }
 
-
-function dialogBox () {
-        console.log("Clicked");
-        setShowModal(true);// Set showModal state to true to display the modal
-}
-
-function closeDialogBox() {
-    setShowModal(false); // Set showModal state to false to hide the modal
-}
-
-
     return (
         <div className="container mt-4 text-center">
             <button className="btn btn-primary mb-3" onClick={handleSubmit}>List Courses</button>
-            {showModal && (
-                <Modal onClose={closeDialogBox} onDelete={() => handleDelete(selectedCourse)} />
-            )}
             <div>
                 <table className="table table-striped-columns">
                     <thead>
@@ -80,7 +66,7 @@ function closeDialogBox() {
                                 <td>{course.courseTitle}</td>
                                 <td>{course.courseCode}</td>
                                 <td>
-                                    <span className="me-2"><button className="btn btn-outline-secondary" type="button" onClick={dialogBox}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                    <span className="me-2"><button className="btn btn-outline-secondary" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                                     </svg></button></span>
 
